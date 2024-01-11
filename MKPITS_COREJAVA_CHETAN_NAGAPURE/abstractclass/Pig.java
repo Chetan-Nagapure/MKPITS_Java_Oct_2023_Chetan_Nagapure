641699a04d8cb925e1ce753801985ad3ab3a9ec9
admin <admin@DESKTOP-0KPJQJA>
2024-01-11 12:23:02 +0530

    New file added

---
+++ MKPITS_COREJAVA_CHETAN_NAGAPURE/abstractclass/Pig.java
@@ -0,0 +1,12 @@
+package com.mkpits.abstractclass;
+
+public class Pig extends Animal 
+{
+//Sub Class
+	public void sound() 
+	{
+		System.out.println("Zee Zee");
+
+	}
+
+}
---
+++ MKPITS_COREJAVA_CHETAN_NAGAPURE/interface1/Dog.java
@@ -0,0 +1,17 @@
+package com.mkpits.interface1;
+
+public class Dog implements Animal1 {
+	public void animalsounds() 
+	{
+		System.out.println("Dog sound is : Bhoo - Bhoo");
+
+	}
+
+	
+	public void sleep() 
+	
+	{
+		System.out.println("sssss");
+	}
+
+}
